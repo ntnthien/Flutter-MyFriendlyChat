8cdd64f2e508593ba9971eb0833c4a6a24d04881
Do Nguyen <daniel@zincer.com>
2018-03-26 01:05:13 +0700

    add TextComposer

--- lib/main.dart
+++ lib/main.dart
@@ -6,25 +6,41 @@ class FriendlychatApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Friendlychat',
-      home: new ChatScreen()
-    );
+    return new MaterialApp(title: 'Friendlychat', home: new ChatScreen());
   }
 }
 
 class ChatScreen extends StatefulWidget {
- @override
-   State<StatefulWidget> createState() => new ChatScreenState();
+  @override
+  State<StatefulWidget> createState() => new ChatScreenState();
 }
 
 class ChatScreenState extends State<ChatScreen> {
-   @override
-    Widget build(BuildContext context) {
-      return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Friendlychat"),
-        ),
-      )
-    }
+  final TextEditingController _textController =
+      new TextEditingController(); //new
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Friendlychat"),
+      ),
+      body: _buildTextComposer(), //new
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: new InputDecoration.collapsed(hintText: "Send a message"),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
 }
